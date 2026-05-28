01add824599c704a552ea944a1f9609908fcd31b
Chuhouan <chuhouan@users.noreply.github.com>
2026-05-28 21:07:53 +0800

    Configure basePath for GitHub Pages subpath

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'export',
+  basePath: '/election-map-2015',
+  assetPrefix: '/election-map-2015/',
   images: {
     unoptimized: true,
   },
